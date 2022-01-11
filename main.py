701921f279cf2cbd79d19d40f4dc9f2bae93c7c5
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-11 19:30:38 +0800

    startscreen #1

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from tkinter import *
 
 root = Tk()
 
+#title
+root.title('SimplEvents')
+root.iconbitmap('')
+root.geometry("500x500")
 #creating label widget
 label1 = Label(root, text="SimplEvents")
+label2 = Label(root, text = "Events Made Simple")
 #putting widget on screen
-label1.pack()
+label1.grid(row=0, column = 1)
+label2.grid(row=1, column = 1)
+#loop
+root.mainloop()
+
