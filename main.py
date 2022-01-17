f837ba3c0f89f6b23d059216df6a92eb0f906982
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-17 22:57:30 +0800

    button positioning 1(mainframe)

--- main.py
+++ main.py
@@ -1,63 +1,26 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import font
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
+root.geometry('1920x1080')
 root.title('SimplEvents')
+root.attributes("-fullscreen", True)
 
-#fullscreen
-#root.attributes('-fullscreen', True)
+mainframe = Frame(root, bg = 'white')
+mainframe.pack(expand = True, fill = BOTH)
 
-mainframe = Frame(root)
-#neweventdaysframe = Frame(root)
-
-mainframe = LabelFrame(root, text = "mainframe", padx = 1920, pady = 1080)
-mainframe.pack(padx = 10, pady = 10)
-
-'''
-def change_to_neweventdaysframe():
-    neweventdaysframe.pack(fill='both', expand=1)
-    mainframe.pack_forget()
-'''
-
-neweventbutton = Button(mainframe, text = "New Event", font=("Arial", 25))
+neweventbutton = Button(mainframe, text = "New Event", width = 136, height = 75,)
 #neweventbutton.grid(row=0,column=0)
 
-neweventbutton.place(x = 20, y = 30, width=120, height=25)
-Button(neweventbutton.master,
-              text="New Event",
-  
-              #font size
-              font=("Arial", 25)
-              ).pack()
+#neweventbutton.place(x = 20, y = 30, width=120, height=25)
+neweventbutton.grid(row = 0, column = 0,  sticky = 'nsew')
 
-'''
-neweventdaysframe = LabelFrame(root, padx = 1920, pady = 1080)
-neweventdaysframe.pack(padx = 10, pady = 10)
+calendarbutton = Button(mainframe, text = "Calendar", width = 136, height = 40)
+calendarbutton.grid(row = 0, column = 1,  sticky = 'nsew')
 
-neweventonedaybutton = Button(neweventdaysframe, text = "One Day")
-neweventonedaybutton.grid(row=0,column=0)
-
-neweventmultdaysbutton = Button(neweventdaysframe, text = "Multiple Days")
-neweventmultdaysbutton.grid(row=0,column=1)
-'''
+aboutusbutton = Button(mainframe, text = 'About Us', width = 136, height = 35)
+aboutusbutton.grid(row = 2, column = 1,  sticky = 'nsew')
 
 root.mainloop()
-
-'''
-#title
-root.title('SimplEvents')
-root.iconbitmap('')
-root.geometry("1280x720")
-root.configure(bg='grey')
-
-def neweventfunction():
-    label3 = Button(root, text= "Same day", fg='blue')
-    label4 = Button(root, text= "Multiple days", fg='blue')
-
-#creating label widget
-newEventButton = Button(root, text="New Event", command=neweventfunction, fg='black', bg='green',padx=150,pady=350) 
-newEventButton.grid(row=0, column=0)
-
-root.mainloop()
-'''
