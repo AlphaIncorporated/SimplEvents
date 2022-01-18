94266244c83a7e6b3283b809073f68c33cb1c89f
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-18 23:22:54 +0800

    mainframe complete 1

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ self.attributes("-fullscreen", True)
 mainframe = Frame(self, bg = 'red')
 mainframe.pack(expand = True, fill = BOTH)
 
+#image declaring
 calendarimg = PhotoImage(file = "pictures/calenderbuttonpic.png", width = 970, height = 1190)
+neweventimg = PhotoImage(file = "pictures/neweventbuttonpic.png", width = 970, height = 605)
+aboutusimg = PhotoImage(file = "pictures/aboutusbuttonpic.png", width = 970, height = 555)
 
-neweventbutton = Button(mainframe, text = "Calendar", width = 136, height = 40, borderwidth=0, bg = 'white')
+calendarbutton = Button(mainframe, text = "Calendar", width = 965, height = 1136, borderwidth=0, image = calendarimg, bg = 'white')
+calendarbutton.grid(row = 0, column = 0, rowspan = 2, sticky = W)
+
+neweventbutton = Button(mainframe, text = "New Event", width = 957, height = 575, borderwidth=0, image = neweventimg, bg = 'white')
 neweventbutton.grid(row = 0, column = 1)
 
-aboutusbutton = Button(mainframe, text = 'About Us', width = 136, height = 35, borderwidth=0, bg = 'white')
+aboutusbutton = Button(mainframe, text = 'About Us', width = 957, height = 560, borderwidth=0, image = aboutusimg, bg = 'white')
 aboutusbutton.grid(row = 1, column = 1, sticky = S )
-
-calendarbutton = Button(mainframe, text = "New Event", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white')
-calendarbutton.grid(row = 0, column = 0, rowspan = 2, sticky = W)
 
 self.mainloop()
