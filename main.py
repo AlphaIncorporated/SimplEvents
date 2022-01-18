24c0834dc6478a183db5530154938effae40d9ef
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-18 19:42:05 +0800

    tkinter mainframe buttons

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter import font
 import tkinter as tk
+from tkinter_custom_button import TkinterCustomButton
 
 root = tk.Tk()
 root.geometry('1920x1080')
@@ -11,16 +10,16 @@ root.attributes("-fullscreen", True)
 mainframe = Frame(root, bg = 'white')
 mainframe.pack(expand = True, fill = BOTH)
 
-neweventbutton = Button(mainframe, text = "New Event", width = 136, height = 75,)
+calendarbutton = Button(mainframe, text = "Calendar", width = 136, height = 40)
+calendarbutton.grid(row = 0, column = 1,  sticky = W)
+
+aboutusbutton = Button(mainframe, text = 'About Us', width = 136, height = 35)
+aboutusbutton.grid(row = 1, column = 1)
+
+neweventbutton = Button(mainframe, text = "New Event", width = 136, height = 75)
 #neweventbutton.grid(row=0,column=0)
 
 #neweventbutton.place(x = 20, y = 30, width=120, height=25)
-neweventbutton.grid(row = 0, column = 0,  sticky = 'nsew')
-
-calendarbutton = Button(mainframe, text = "Calendar", width = 136, height = 40)
-calendarbutton.grid(row = 0, column = 1,  sticky = 'nsew')
-
-aboutusbutton = Button(mainframe, text = 'About Us', width = 136, height = 35)
-aboutusbutton.grid(row = 2, column = 1,  sticky = 'nsew')
+neweventbutton.grid(row = 0, column = 0, rowspan = 2)
 
 root.mainloop()
