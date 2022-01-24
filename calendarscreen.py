882d7297ae1ea9d9dcce90bf54ca761c0f73c2e6
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-25 00:22:54 +0800

    calendar.py fix

--- calendarscreen.py
+++ calendarscreen.py
@@ -1,22 +1,13 @@
 from tkinter import *
 
-def CalendarUI(self):  
-    calendarframe = Frame(self.mainframe)
-    calendarframe.pack(expand = True, fill = BOTH)
-
-    self.calendarbackbutton = Button(calendarframe, text = "Back")
-    self.calendarbackbutton.grid(row = 0, column = 0)
-
-
-root = Tk()
-root.geometry('2304x1440')
-root.title('SimplEvents')
-root.grid_columnconfigure(0, weight = 1)
-root.grid_rowconfigure(0, weight = 1)   
-root.attributes('-fullscreen', True)
-
-
 class CalendarUI:
+    
+    root = Tk()
+    root.geometry('2304x1440')
+    root.title('SimplEvents')
+    root.grid_columnconfigure(0, weight = 1)
+    root.grid_rowconfigure(0, weight = 1)   
+    root.attributes('-fullscreen', True)
     
     def __init__(self, master):
         calendarframe = Frame(master)
@@ -28,8 +19,5 @@ class CalendarUI:
     #def changetomainframe():
            # mainframe.pack(expand = True, fill = BOTH)
            # calendarframe.forget()
-            
-calendar = CalendarUI(root)
 
-
-root.mainloop()
+    root.mainloop()
