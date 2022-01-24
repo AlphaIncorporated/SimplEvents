eb37313966774f44860a02db7916343ddcb31945
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-01-24 17:46:39 +0800

    restart frame change code

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 from tkinter import *
-from calendarscreen import CalendarUI
 
-root = Tk()
-root.geometry('2304x1440')
-root.title('SimplEvents')
-root.grid_columnconfigure(0, weight = 1)
-root.grid_rowconfigure(0, weight = 1)
-root.attributes('-fullscreen', True)
+#screen
+self = Tk()
+self.geometry('2304x1440')
+self.title('SimplEvents')
+self.grid_columnconfigure(0, weight = 1)
+self.grid_rowconfigure(0, weight = 1)
+self.attributes('-fullscreen', True)
 
-class App():
-    def __init__(self, parent):
-        self.setupWidgets(self)
-    
-    def setupWidgets(self):
-        self.mainframe = Frame(self, bg = 'red')
-        self.mainframe.pack(expand = True, fill = BOTH)
+#mainFrame
+mainframe = Frame(self, bg = 'red')
+mainframe.pack(expand = True, fill = BOTH)
 
-        self.mainframe.grid_rowconfigure(0, weight=0) # For row 0
-        self.mainframe.grid_rowconfigure(1, weight=2) # For row 1
-        self.mainframe.grid_columnconfigure(0, weight=1) # For column 0
-        self.mainframe.grid_columnconfigure(1, weight=1) # For column 1
+#grid configs
+mainframe.grid_rowconfigure(0, weight=0) # For row 0
+mainframe.grid_rowconfigure(1, weight=2) # For row 1
+mainframe.grid_columnconfigure(0, weight=1) # For column 0
+mainframe.grid_columnconfigure(1, weight=1) # For column 1
 
-        #change frame functions
-        def changetocalendar(self): 
-            self.mainframe.forget()
-            CalendarUI(self)
-            
-        def changetomainframe(self):
-            self.mainframe.pack(expand = True, fill = BOTH)
-            self.calendarframe.forget()
-                
-        #image declaring
-        calendarimg = PhotoImage(file = "./pictures/calenderbuttonpic.png")
-        neweventimg = PhotoImage(file = "./pictures/neweventbuttonpic.png")
-        aboutusimg = PhotoImage(file = "./pictures/aboutusbuttonpic.png")
+#mainframe image declaring
+calendarimg = PhotoImage(file = "./pictures/calenderbuttonpic.png")
+neweventimg = PhotoImage(file = "./pictures/neweventbuttonpic.png")
+aboutusimg = PhotoImage(file = "./pictures/aboutusbuttonpic.png")
 
-        calendarbutton = Button(self.mainframe, text = "Calendar", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white', command=lambda: self.changetocalendar(self))
-        calendarbutton.grid(row = 0, column = 0, rowspan = 2)
+#mainframe buttons
+calendarbutton = Button(mainframe, text = "Calendar", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white')
+calendarbutton.grid(row = 0, column = 0, rowspan = 2)
 
-        neweventbutton = Button(self.mainframe, text = "New Event", width = 960, height = 575, borderwidth=0, image = neweventimg, bg = 'white')
-        neweventbutton.grid(row = 0, column = 1)
+neweventbutton = Button(mainframe, text = "New Event", width = 960, height = 575, borderwidth=0, image = neweventimg, bg = 'white')
+neweventbutton.grid(row = 0, column = 1)
 
-        aboutusbutton = Button(self.mainframe, text = 'About Us', width = 957, height = 560, borderwidth=0, image = aboutusimg, bg = 'white')
-        aboutusbutton.grid(row = 1, column = 1)
+aboutusbutton = Button(mainframe, text = 'About Us', width = 957, height = 560, borderwidth=0, image = aboutusimg, bg = 'white')
+aboutusbutton.grid(row = 1, column = 1)
 
-    root.mainloop()
+self.mainloop()
