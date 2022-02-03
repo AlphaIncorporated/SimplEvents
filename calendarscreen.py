de45ad5254400710695ef529c64f51e033fd544e
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-02-03 13:04:38 +0800

    added switching of frames

--- calendarscreen.py
+++ calendarscreen.py
@@ -1,23 +1,20 @@
 from tkinter import *
 
-class CalendarUI:
+root = Tk()
+root.geometry('2304x1440')
+root.title('SimplEvents')
+root.grid_columnconfigure(0, weight = 1)
+root.grid_rowconfigure(0, weight = 1)   
+root.attributes('-fullscreen', True)
     
-    root = Tk()
-    root.geometry('2304x1440')
-    root.title('SimplEvents')
-    root.grid_columnconfigure(0, weight = 1)
-    root.grid_rowconfigure(0, weight = 1)   
-    root.attributes('-fullscreen', True)
-    
-    def __init__(self, master):
-        calendarframe = Frame(master)
-        calendarframe.pack(expand = True, fill = BOTH)
-    
-        self.calendarbackbutton = Button(calendarframe, text = "Back")
-        #, command = self.changetomainframe
-        self.calendarbackbutton.grid(row = 0, column = 0)
-    #def changetomainframe():
-           # mainframe.pack(expand = True, fill = BOTH)
-           # calendarframe.forget()
+calendarframe = Frame(root)
+calendarframe.pack(expand = True, fill = BOTH)
 
-    root.mainloop()
+#def switchtomainframe():
+    #root.destroy()
+    #import main
+
+#root.calendarbackbutton = Button(calendarframe, text = "Back")
+#root.calendarbackbutton.grid(row = 0, column = 0)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 
 root = Tk()
 root.geometry('2304x1440')
@@ -10,6 +11,10 @@ root.attributes('-fullscreen', True)
 root.mainframe = Frame(root, bg = 'red')
 root.mainframe.pack(expand = True, fill = BOTH)
 
+def switchtocalendarframe():
+    root.destroy()
+    import calendarscreen
+
 root.mainframe.grid_rowconfigure(0, weight=0) # For row 0
 root.mainframe.grid_rowconfigure(1, weight=2) # For row 1
 root.mainframe.grid_columnconfigure(0, weight=1) # For column 0
@@ -19,8 +24,8 @@ root.mainframe.grid_columnconfigure(1, weight=1) # For column 1
 calendarimg = PhotoImage(file = "./pictures/calenderbuttonpic.png")
 neweventimg = PhotoImage(file = "./pictures/neweventbuttonpic.png")
 aboutusimg = PhotoImage(file = "./pictures/aboutusbuttonpic.png")
-
-calendarbutton = Button(root.mainframe, text = "Calendar", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white')
+ 
+calendarbutton = Button(root.mainframe, text = "Calendar", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white', command = switchtocalendarframe)
 calendarbutton.grid(row = 0, column = 0, rowspan = 2)
 
 neweventbutton = Button(root.mainframe, text = "New Event", width = 960, height = 575, borderwidth=0, image = neweventimg, bg = 'white')
