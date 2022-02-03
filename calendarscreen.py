f2df977bf800b522928dca21480894f7fbb9a44c
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-02-03 23:24:47 +0800

    Adding first few UI for calendar

--- calendarscreen.py
+++ calendarscreen.py
@@ -1,15 +1,19 @@
 from tkinter import *
-    
+from calendar import monthrange
+from datetime import datetime
 
 def calendarScreen(self):
-    calendarframe = Frame(self.mainFrame)
+    calendarframe = Frame(self.mainFrame, bg = 'white')
     calendarframe.pack(expand = True, fill = BOTH)
-    tempText = Label(calendarframe, text="This is a test") # Can be removed (Ethan)
-    tempText.pack()
 
-#def switchtomainframe():
-    #root.destroy()
-    #import main
-
-#root.calendarbackbutton = Button(calendarframe, text = "Back")
-#root.calendarbackbutton.grid(row = 0, column = 0)
+    currentDay = datetime.now().day
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
+    
+    if monthrange(currentYear, currentMonth)[1] == 28:
+        day = dict()
+        for x in range(1, 29):
+            day[x] = x + 1
+            day[x] = Button(calendarframe, text = x, width = 25, height = 10)
+            day[x].grid(row = 0, column = x + 1)
+            
