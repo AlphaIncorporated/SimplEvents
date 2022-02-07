25f461a4d5cab92369e33cbeddc05f747544d4ee
Alfred-Kang <alfred_kang_jing_rui@s2019.ssts.edu.sg>
2022-02-08 00:05:04 +0800

    backbutton

--- calendarscreen.py
+++ calendarscreen.py
@@ -3,6 +3,11 @@ from calendar import monthrange
 from datetime import datetime
 
 def calendarScreen(self):
+    def back(self, calendarframe):
+        self.uiFrame.pack(expand = True, fill = BOTH)
+        calendarframe.pack_forget()
+    length=self.winfo_screenwidth()/10.5
+    height=self.winfo_screenheight()/10.5
     calendarframe = Frame(self.mainFrame, bg = 'white')
     calendarframe.pack(expand = True, fill = BOTH)
 
@@ -10,29 +15,33 @@ def calendarScreen(self):
     currentDay = datetime.now().day
     currentMonth = datetime.now().month
     currentYear = datetime.now().year
+    
+    #back button
+    backbutton = Button(calendarframe, bg = 'white', text = 'Back', font = ("Ariel, 50"), fg = "black", borderwidth = 0 ,command=(lambda: back(self,calendarframe)))
+    backbutton.grid(row = 0, column = 0)
 
     #Calendar Title
-    calendartitle = Label(calendarframe, text = "Calendar", font = ("Ariel, 50"), bg = 'white')
-    calendartitle.grid(row = 0, column = 0, columnspan=5, pady = 10)
+    calendartitle = Label(calendarframe, text = "Calendar", font = ("Ariel, 50"), bg = 'white', fg = "black")
+    calendartitle.grid(row = 0, column = 1, columnspan=4, pady = 10)
 
     #neweventbutton
-    calendartoneweventbutton = Button(calendarframe, text = "New Event", width = 44, height = 5, bg = 'white', borderwidth = 1)
+    calendartoneweventbutton = Button(calendarframe, text = "New Event", width = 44, height = 5)
     calendartoneweventbutton.grid(row = 0, column = 5, columnspan=2, sticky = E, pady = 10)
 
     #Day of the Week Label
-    calendardayslabelmonday = Label(calendarframe, text = 'Monday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelmonday = Label(calendarframe, text = 'Monday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelmonday.grid(row = 2, column = 0)
-    calendardayslabeltuesday = Label(calendarframe, text = 'Tuesday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabeltuesday = Label(calendarframe, text = 'Tuesday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabeltuesday.grid(row = 2, column = 1)
-    calendardayslabelwednesday = Label(calendarframe, text = 'Wednesday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelwednesday = Label(calendarframe, text = 'Wednesday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelwednesday.grid(row = 2, column = 2)
-    calendardayslabelthursday = Label(calendarframe, text = 'Thursday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelthursday = Label(calendarframe, text = 'Thursday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelthursday.grid(row = 2, column = 3)
-    calendardayslabelfriday = Label(calendarframe, text = 'Friday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelfriday = Label(calendarframe, text = 'Friday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelfriday.grid(row = 2, column = 4)
-    calendardayslabelsaturday = Label(calendarframe, text = 'Saturday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelsaturday = Label(calendarframe, text = 'Saturday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelsaturday.grid(row = 2, column = 5)
-    calendardayslabelsunday = Label(calendarframe, text = 'Sunday', font = ("Ariel, 30"), bg = 'white')
+    calendardayslabelsunday = Label(calendarframe, text = 'Sunday', font = ("Ariel, 30"), bg = 'white', fg = "black")
     calendardayslabelsunday.grid(row = 2, column = 6)
     
     #making daybuttons
@@ -41,6 +50,54 @@ def calendarScreen(self):
         calendarrow = 3
         calendarcolumn = 0
         for x in range(1, 29):
+            day[x] = x + 1  
+            day[x] = Button(calendarframe, text = x, width = int(length/7), height = int(height/10), borderwidth = 0, bg = 'white')
+            if calendarcolumn > 6:
+                calendarrow = calendarrow + 2
+                calendarcolumn = 0
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+            else:
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+
+    if monthrange(currentYear, currentMonth)[1] == 29:
+        day = dict()
+        calendarrow = 3
+        calendarcolumn = 0
+        for x in range(1, 30):
+            day[x] = x + 1
+            day[x] = Button(calendarframe, text = x, width = 22, height = 9, borderwidth = 0, bg = 'white')
+            if calendarcolumn > 6:
+                calendarrow = calendarrow + 2
+                calendarcolumn = 0
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+            else:
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+
+    if monthrange(currentYear, currentMonth)[1] == 30:
+        day = dict()
+        calendarrow = 3
+        calendarcolumn = 0
+        for x in range(1, 31):
+            day[x] = x + 1
+            day[x] = Button(calendarframe, text = x, width = 22, height = 9, borderwidth = 0, bg = 'white')
+            if calendarcolumn > 6:
+                calendarrow = calendarrow + 2
+                calendarcolumn = 0
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+            else:
+                day[x].grid(row = calendarrow, column = calendarcolumn, rowspan = '2')
+                calendarcolumn = calendarcolumn + 1
+
+    if monthrange(currentYear, currentMonth)[1] == 31:
+        day = dict()
+        calendarrow = 3
+        calendarcolumn = 0
+        for x in range(1, 32):
             day[x] = x + 1
             day[x] = Button(calendarframe, text = x, width = 22, height = 9, borderwidth = 0, bg = 'white')
             if calendarcolumn > 6:
--- main.py
+++ main.py
@@ -2,23 +2,29 @@ from tkinter import *
 import tkinter as tk
 
 from calendarscreen import calendarScreen
-
+from newEventForm import newEventForm
 root = Tk()
-root.geometry('2304x1440')
+# root.geometry('2304x1440')
 root.title('SimplEvents')
 root.grid_columnconfigure(0, weight = 1)
 root.grid_rowconfigure(0, weight = 1)
 root.attributes('-fullscreen', True)
 
-root.mainFrame = Frame(root) # A new frame to 'govern' and contain all other frames
+def new_func(root):
+    root.mainFrame = Frame(root) # A new frame to 'govern' and contain all other frames
+
+new_func(root)
 root.mainFrame.pack()
 root.uiFrame = Frame(root.mainFrame, bg = 'red')
 root.uiFrame.pack(expand = True, fill = BOTH)
 
 def switchtocalendarframe(root):
     root.uiFrame.pack_forget()
-    calendarScreen(root) # Calls the Function in CalendarScreen.py
-
+    calendarScreen(root)
+def switchtoneweventframe(root):
+    root.uiFrame.pack_forget()
+    calendarScreen(root)
+    newEventFormObj = newEventForm(root).place(in_=root, anchor="c", relx=.5, rely=.5, relheight=1, relwidth=1)
 root.uiFrame.grid_rowconfigure(0, weight=0) # For row 0
 root.uiFrame.grid_rowconfigure(1, weight=2) # For row 1
 root.uiFrame.grid_columnconfigure(0, weight=1) # For column 0
@@ -31,8 +37,15 @@ aboutusimg = PhotoImage(file = "./pictures/aboutusbuttonpic.png")
  
 calendarbutton = Button(root.uiFrame, text = "Calendar", width = 965, height = 1135, borderwidth=0, image = calendarimg, bg = 'white', command = lambda: switchtocalendarframe(root))
 calendarbutton.grid(row = 0, column = 0, rowspan = 2)
-
-neweventbutton = Button(root.uiFrame, text = "New Event", width = 960, height = 575, borderwidth=0, image = neweventimg, bg = 'white')
+neweventbutton = Button(
+    root.uiFrame,
+    text = "New Event",
+    width = 960,
+    height = 575,
+    borderwidth=0,  
+    image = neweventimg,
+    bg = 'white',
+    command = lambda: switchtoneweventframe(root))
 neweventbutton.grid(row = 0, column = 1)
 
 aboutusbutton = Button(root.uiFrame, text = 'About Us', width = 957, height = 560, borderwidth=0, image = aboutusimg, bg = 'white')
